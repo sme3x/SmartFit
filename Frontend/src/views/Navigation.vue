4f1e7822785b85d0273fb79d8594d1533f4a8e3e
Tim Medves <tim.medvess@gmail.com>
2023-10-28 18:49:55 +0200

    Clear unused code and create structure of code (modular development)

---
+++ Frontend/src/views/Navigation.vue
@@ -0,0 +1,52 @@
+<script setup>
+  import { ref } from 'vue'
+
+  const links = [
+    ['mdi-inbox-arrow-down', 'Inbox'],
+    ['mdi-send', 'Send'],
+    ['mdi-delete', 'Trash'],
+    ['mdi-alert-octagon', 'Spam'],
+  ]
+
+  const drawer = ref(null)
+</script>
+
+<template>
+    <!-- <v-system-bar>
+      <v-spacer></v-spacer>
+      <v-icon>mdi-square</v-icon>
+      <v-icon>mdi-circle</v-icon>
+      <v-icon>mdi-triangle</v-icon>
+    </v-system-bar> -->
+    <v-app-bar color="blue">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-title>Application</v-app-bar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer">
+      <v-sheet
+        color="grey-lighten-4"
+        class="pa-4"
+      >
+        <v-avatar
+          class="mb-4"
+          color="grey-darken-1"
+          size="64"
+        ></v-avatar>
+
+        <div>john@google.com</div>
+      </v-sheet>
+
+      <v-divider></v-divider>
+
+      <v-list>
+        <v-list-item
+          v-for="[icon, text] in links"
+          :key="icon"
+          :prepend-icon="icon"
+          :title="text"
+          link
+        ></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+</template>
